1e959964e21b57fd20250cc20f4dc227fac7f46f
Seth Baxter <sethbaxter@prominentedge.com>
2016-01-21 17:13:33 -0500

    Store artifact in s3

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-mvn clean install
+pushd `dirname $0` > /dev/null
+base=$(pwd -P)
+popd > /dev/null
+
+# Build Spring-boot JAR
+mvn clean package
+
+# Path to output JAR
+jarfile=$base/../target/piazzaServiceController*.jar
+
+# Gather some data about the repo
+source $base/vars.sh
+
+# Do we have this artifact in s3? If not, upload it.
+aws s3 ls $S3URL || aws s3 cp $jarfile $S3URL
+
+# success if we have an artifact stored in s3.
+aws s3 ls $S3URL

610f8c7cccc6b666c67156447658f86e8eed41e4
Marge Lynum <mlynum@administrators-MacBook-Pro-2.local>
2016-02-09 15:12:26 -0500

    sample test to do black box testing

---
+++ scripts/black-box-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+
+pushd `dirname $0` > /dev/null
+base=$(pwd -P)
+popd > /dev/null
+
+# Gather some data about the repo
+source $base/vars.sh
+
+#Run the test
+newman -c $base/tests/pz-register-test.json
+

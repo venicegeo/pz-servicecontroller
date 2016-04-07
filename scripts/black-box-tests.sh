931408307c8499516fa6ed5a1f714f0a668f6435
Marge Lynum <mmlynum@gmail.com>
2016-04-07 09:23:54 -0400

    Updated to remove old test

--- scripts/black-box-tests.sh
+++ scripts/black-box-tests.sh
@@ -9,6 +9,5 @@ source $base/vars.sh
 
 #Run the test
 newman -sc $base/tests/PiazzaDevelopment.json.postman_collection
-newman -sc $base/tests/pz-register-test.json
 newman -sc $base/tests/testServiceControllerRestServicesSeq.json.postman_collection
 newman -sc $base/tests/UUID_Logger.json.postman_collection

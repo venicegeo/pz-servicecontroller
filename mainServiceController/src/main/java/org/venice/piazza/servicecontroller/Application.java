563b1e9c43e954bc9c0fb84ea189d1e9e8db576a
Patrick D <patrick.doody@rgi-corp.com>
2016-08-18 08:56:16 -0400

    Updating Service Metadata; Add Validation Bean

--- mainServiceController/src/main/java/org/venice/piazza/servicecontroller/Application.java
+++ mainServiceController/src/main/java/org/venice/piazza/servicecontroller/Application.java
@@ -14,6 +14,7 @@
  * limitations under the License.
  *******************************************************************************/
 package org.venice.piazza.servicecontroller;
+
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
@@ -22,11 +23,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
 /**
- * Main class for the pz-servicecontroller.  Launches the application
+ * Main class for the pz-servicecontroller. Launches the application
+ * 
  * @author mlynum
  * @since 1.0
  */
@@ -37,48 +41,48 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableMongoRepositories("org.venice.piazza.serviceregistry.data.mongodb.repository")
 /* Enable Boot application and MongoRepositories */
 public class Application extends SpringBootServletInitializer {
-	
-    
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		
+
 		return builder.sources(Application.class);
 	}
 
 	public static void main(String[] args) {
-		
 
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
-		
-		
+
 		// now check to see if the first parameter is true, if so then test the health of the
 		// Spring environment
 		if (args.length == 1) {
-            // Get the value of the first argument
+			// Get the value of the first argument
 			// If it is true then do a health check and print it out
 			Boolean inspectBool = Boolean.valueOf(args[0]);
 			if (inspectBool.booleanValue() == true) {
 				inspectSprintEnv(ctx);
 			}
-		}               
-		
+		}
+
 	}
-	
+
 	/**
 	 * Determines if the appropriate bean definitions are available
 	 */
 	public static void inspectSprintEnv(ApplicationContext ctx) {
-		
+
 		System.out.println("Spring Boot Beans");
 		System.out.println("-----------------");
 
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-		
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean getLocalValidatorFactoryBean() {
+		return new LocalValidatorFactoryBean();
 	}
 }
-
-
